bitcoin-core/secp256k1

Use explicit symbol visibility.
The use of static makes this somewhat redundant currently, though if
 we later have multiple compilation units it will be needed.

This also sets the dllexport needed for shared libraries on win32.

include/secp256k1_ecdh.h
@@ -16,7 +16,7 @@ extern "C" {
  *  In:      point:    pointer to a public point
  *           scalar:   a 32-byte scalar with which to multiply the point
  */
-SECP256K1_WARN_UNUSED_RESULT int secp256k1_ecdh(
+SECP256K1_EXPORT SECP256K1_WARN_UNUSED_RESULT int secp256k1_ecdh(
   const secp256k1_context_t* ctx,
   unsigned char *result,
   const secp256k1_pubkey_t *point,

include/secp256k1_recovery.h
@@ -33,7 +33,7 @@ typedef struct {
  *  In:   input64: a pointer to a 64-byte compact signature
  *        recid:   the recovery id (0, 1, 2 or 3)
  */
-int secp256k1_ecdsa_recoverable_signature_parse_compact(
+SECP256K1_EXPORT int secp256k1_ecdsa_recoverable_signature_parse_compact(
     const secp256k1_context_t* ctx,
     secp256k1_ecdsa_recoverable_signature_t* sig,
     const unsigned char *input64,
@@ -46,7 +46,7 @@ int secp256k1_ecdsa_recoverable_signature_parse_compact(
  *  Out: sig:    a pointer to a normal signature (cannot be NULL).
  *  In:  sigin:  a pointer to a recoverable signature (cannot be NULL).
  */
-int secp256k1_ecdsa_recoverable_signature_convert(
+SECP256K1_EXPORT int secp256k1_ecdsa_recoverable_signature_convert(
     const secp256k1_context_t* ctx,
     secp256k1_ecdsa_signature_t* sig,
     const secp256k1_ecdsa_recoverable_signature_t* sigin
@@ -60,7 +60,7 @@ int secp256k1_ecdsa_recoverable_signature_convert(
  *        recid:    a pointer to an integer to hold the recovery id (can be NULL).
  *  In:   sig:      a pointer to an initialized signature object (cannot be NULL)
  */
-int secp256k1_ecdsa_recoverable_signature_serialize_compact(
+SECP256K1_EXPORT int secp256k1_ecdsa_recoverable_signature_serialize_compact(
     const secp256k1_context_t* ctx,
     unsigned char *output64,
     int *recid,
@@ -78,7 +78,7 @@ int secp256k1_ecdsa_recoverable_signature_serialize_compact(
  *           noncefp:pointer to a nonce generation function. If NULL, secp256k1_nonce_function_default is used
  *           ndata:  pointer to arbitrary data used by the nonce generation function (can be NULL)
  */
-int secp256k1_ecdsa_sign_recoverable(
+SECP256K1_EXPORT int secp256k1_ecdsa_sign_recoverable(
     const secp256k1_context_t* ctx,
     secp256k1_ecdsa_recoverable_signature_t *sig,
     const unsigned char *msg32,
@@ -96,7 +96,7 @@ int secp256k1_ecdsa_sign_recoverable(
  *  In:      sig:        pointer to initialized signature that supports pubkey recovery (cannot be NULL)
  *           msg32:      the 32-byte message hash assumed to be signed (cannot be NULL)
  */
-SECP256K1_WARN_UNUSED_RESULT int secp256k1_ecdsa_recover(
+SECP256K1_EXPORT SECP256K1_WARN_UNUSED_RESULT int secp256k1_ecdsa_recover(
     const secp256k1_context_t* ctx,
     secp256k1_pubkey_t *pubkey,
     const secp256k1_ecdsa_recoverable_signature_t *sig,

include/secp256k1_schnorr.h
@@ -24,7 +24,7 @@ extern "C" {
  *           ndata:  pointer to arbitrary data used by the nonce generation
  *                   function (can be NULL)
  */
-int secp256k1_schnorr_sign(
+SECP256K1_EXPORT int secp256k1_schnorr_sign(
   const secp256k1_context_t* ctx,
   unsigned char *sig64,
   const unsigned char *msg32,
@@ -41,7 +41,7 @@ int secp256k1_schnorr_sign(
  *           msg32:     the 32-byte message hash being verified (cannot be NULL)
  *           pubkey:    the public key to verify with (cannot be NULL)
  */
-SECP256K1_WARN_UNUSED_RESULT int secp256k1_schnorr_verify(
+SECP256K1_EXPORT SECP256K1_WARN_UNUSED_RESULT int secp256k1_schnorr_verify(
   const secp256k1_context_t* ctx,
   const unsigned char *sig64,
   const unsigned char *msg32,
@@ -61,7 +61,7 @@ SECP256K1_WARN_UNUSED_RESULT int secp256k1_schnorr_verify(
  *           msg32:      the 32-byte message hash assumed to be signed (cannot
  *                       be NULL)
  */
-int secp256k1_schnorr_recover(
+SECP256K1_EXPORT int secp256k1_schnorr_recover(
   const secp256k1_context_t* ctx,
   secp256k1_pubkey_t *pubkey,
   const unsigned char *sig64,
@@ -86,7 +86,7 @@ int secp256k1_schnorr_recover(
  *
  *  Do not use the output as a private/public key pair for signing/validation.
  */
-int secp256k1_schnorr_generate_nonce_pair(
+SECP256K1_EXPORT int secp256k1_schnorr_generate_nonce_pair(
   const secp256k1_context_t* ctx,
   secp256k1_pubkey_t *pubnonce,
   unsigned char *privnonce32,
@@ -138,7 +138,7 @@ int secp256k1_schnorr_generate_nonce_pair(
  *  pre-combine several inputs already with one call, and add more inputs later
  *  by calling the function again (they are commutative and associative).
  */
-SECP256K1_WARN_UNUSED_RESULT int secp256k1_schnorr_partial_sign(
+SECP256K1_EXPORT SECP256K1_WARN_UNUSED_RESULT int secp256k1_schnorr_partial_sign(
   const secp256k1_context_t* ctx,
   unsigned char *sig64,
   const unsigned char *msg32,
@@ -159,7 +159,7 @@ SECP256K1_WARN_UNUSED_RESULT int secp256k1_schnorr_partial_sign(
  *                   signatures
  *         n:        the number of signatures to combine (at least 1)
  */
-SECP256K1_WARN_UNUSED_RESULT int secp256k1_schnorr_partial_combine(
+SECP256K1_EXPORT SECP256K1_WARN_UNUSED_RESULT int secp256k1_schnorr_partial_combine(
   const secp256k1_context_t* ctx,
   unsigned char *sig64,
   const unsigned char * const * sig64sin,

Pyrrha-Foundation/pyrrha-coin

include/secp256k1_ecdh.h
@@ -16,7 +16,7 @@ extern "C" {
  *  In:      point:    pointer to a public point
  *           scalar:   a 32-byte scalar with which to multiply the point
  */
-SECP256K1_WARN_UNUSED_RESULT int secp256k1_ecdh(
+SECP256K1_API SECP256K1_WARN_UNUSED_RESULT int secp256k1_ecdh(
   const secp256k1_context* ctx,
   unsigned char *result,
   const secp256k1_pubkey *point,

include/secp256k1_recovery.h
@@ -33,7 +33,7 @@ typedef struct {
  *  In:   input64: a pointer to a 64-byte compact signature
  *        recid:   the recovery id (0, 1, 2 or 3)
  */
-int secp256k1_ecdsa_recoverable_signature_parse_compact(
+SECP256K1_API int secp256k1_ecdsa_recoverable_signature_parse_compact(
     const secp256k1_context* ctx,
     secp256k1_ecdsa_recoverable_signature* sig,
     const unsigned char *input64,
@@ -46,7 +46,7 @@ int secp256k1_ecdsa_recoverable_signature_parse_compact(
  *  Out: sig:    a pointer to a normal signature (cannot be NULL).
  *  In:  sigin:  a pointer to a recoverable signature (cannot be NULL).
  */
-int secp256k1_ecdsa_recoverable_signature_convert(
+SECP256K1_API int secp256k1_ecdsa_recoverable_signature_convert(
     const secp256k1_context* ctx,
     secp256k1_ecdsa_signature* sig,
     const secp256k1_ecdsa_recoverable_signature* sigin
@@ -60,7 +60,7 @@ int secp256k1_ecdsa_recoverable_signature_convert(
  *        recid:    a pointer to an integer to hold the recovery id (can be NULL).
  *  In:   sig:      a pointer to an initialized signature object (cannot be NULL)
  */
-int secp256k1_ecdsa_recoverable_signature_serialize_compact(
+SECP256K1_API int secp256k1_ecdsa_recoverable_signature_serialize_compact(
     const secp256k1_context* ctx,
     unsigned char *output64,
     int *recid,
@@ -78,7 +78,7 @@ int secp256k1_ecdsa_recoverable_signature_serialize_compact(
  *           noncefp:pointer to a nonce generation function. If NULL, secp256k1_nonce_function_default is used
  *           ndata:  pointer to arbitrary data used by the nonce generation function (can be NULL)
  */
-int secp256k1_ecdsa_sign_recoverable(
+SECP256K1_API int secp256k1_ecdsa_sign_recoverable(
     const secp256k1_context* ctx,
     secp256k1_ecdsa_recoverable_signature *sig,
     const unsigned char *msg32,
@@ -96,7 +96,7 @@ int secp256k1_ecdsa_sign_recoverable(
  *  In:      sig:        pointer to initialized signature that supports pubkey recovery (cannot be NULL)
  *           msg32:      the 32-byte message hash assumed to be signed (cannot be NULL)
  */
-SECP256K1_WARN_UNUSED_RESULT int secp256k1_ecdsa_recover(
+SECP256K1_API SECP256K1_WARN_UNUSED_RESULT int secp256k1_ecdsa_recover(
     const secp256k1_context* ctx,
     secp256k1_pubkey *pubkey,
     const secp256k1_ecdsa_recoverable_signature *sig,

include/secp256k1_schnorr.h
@@ -24,7 +24,7 @@ extern "C" {
  *           ndata:  pointer to arbitrary data used by the nonce generation
  *                   function (can be NULL)
  */
-int secp256k1_schnorr_sign(
+SECP256K1_API int secp256k1_schnorr_sign(
   const secp256k1_context* ctx,
   unsigned char *sig64,
   const unsigned char *msg32,
@@ -41,7 +41,7 @@ int secp256k1_schnorr_sign(
  *           msg32:     the 32-byte message hash being verified (cannot be NULL)
  *           pubkey:    the public key to verify with (cannot be NULL)
  */
-SECP256K1_WARN_UNUSED_RESULT int secp256k1_schnorr_verify(
+SECP256K1_API SECP256K1_WARN_UNUSED_RESULT int secp256k1_schnorr_verify(
   const secp256k1_context* ctx,
   const unsigned char *sig64,
   const unsigned char *msg32,
@@ -61,7 +61,7 @@ SECP256K1_WARN_UNUSED_RESULT int secp256k1_schnorr_verify(
  *           msg32:      the 32-byte message hash assumed to be signed (cannot
  *                       be NULL)
  */
-int secp256k1_schnorr_recover(
+SECP256K1_API int secp256k1_schnorr_recover(
   const secp256k1_context* ctx,
   secp256k1_pubkey *pubkey,
   const unsigned char *sig64,
@@ -86,7 +86,7 @@ int secp256k1_schnorr_recover(
  *
  *  Do not use the output as a private/public key pair for signing/validation.
  */
-int secp256k1_schnorr_generate_nonce_pair(
+SECP256K1_API int secp256k1_schnorr_generate_nonce_pair(
   const secp256k1_context* ctx,
   secp256k1_pubkey *pubnonce,
   unsigned char *privnonce32,
@@ -138,7 +138,7 @@ int secp256k1_schnorr_generate_nonce_pair(
  *  pre-combine several inputs already with one call, and add more inputs later
  *  by calling the function again (they are commutative and associative).
  */
-SECP256K1_WARN_UNUSED_RESULT int secp256k1_schnorr_partial_sign(
+SECP256K1_API SECP256K1_WARN_UNUSED_RESULT int secp256k1_schnorr_partial_sign(
   const secp256k1_context* ctx,
   unsigned char *sig64,
   const unsigned char *msg32,
@@ -159,7 +159,7 @@ SECP256K1_WARN_UNUSED_RESULT int secp256k1_schnorr_partial_sign(
  *                   signatures
  *         n:        the number of signatures to combine (at least 1)
  */
-SECP256K1_WARN_UNUSED_RESULT int secp256k1_schnorr_partial_combine(
+SECP256K1_API SECP256K1_WARN_UNUSED_RESULT int secp256k1_schnorr_partial_combine(
   const secp256k1_context* ctx,
   unsigned char *sig64,
   const unsigned char * const * sig64sin,