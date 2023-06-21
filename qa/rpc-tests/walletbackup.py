abc7253aca84a860fbe7187f01265013a9c5929c
Andrea Suisani <sickpig@gmail.com>
2023-06-21 16:54:33 +0200

    Fix walletbackup.py

--- qa/rpc-tests/walletbackup.py
+++ qa/rpc-tests/walletbackup.py
@@ -60,7 +60,7 @@ class WalletBackupTest(BitcoinTestFramework):
 
     def one_send(self, from_node, to_address):
         if (randint(1,2) == 1):
-            amount = Decimal(randint(1,10)) / Decimal(10)
+            amount = Decimal(randint(10,100))
             self.nodes[from_node].sendtoaddress(to_address, amount)
 
     def do_one_round(self):
@@ -112,9 +112,9 @@ class WalletBackupTest(BitcoinTestFramework):
         self.nodes[3].generate(100)
         sync_blocks(self.nodes)
 
-        assert_equal(self.nodes[0].getbalance(), 50)
-        assert_equal(self.nodes[1].getbalance(), 50)
-        assert_equal(self.nodes[2].getbalance(), 50)
+        assert_equal(self.nodes[0].getbalance(), COINBASE_REWARD)
+        assert_equal(self.nodes[1].getbalance(), COINBASE_REWARD)
+        assert_equal(self.nodes[2].getbalance(), COINBASE_REWARD)
         assert_equal(self.nodes[3].getbalance(), 0)
 
         logging.info("Creating transactions")
@@ -146,8 +146,8 @@ class WalletBackupTest(BitcoinTestFramework):
         total = balance0 + balance1 + balance2 + balance3
 
         # At this point, there are 214 blocks (103 for setup, then 10 rounds, then 101.)
-        # 114 are mature, so the sum of all wallets should be 114 * 50 = 5700.
-        assert_equal(total, 5700)
+        # 114 are mature, so the sum of all wallets should be 114 * COINBASE_REWARD.
+        assert_equal(total, 114 * COINBASE_REWARD)
 
         ##
         # Test restoring spender wallets from backups
