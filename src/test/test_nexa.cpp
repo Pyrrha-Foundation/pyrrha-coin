ab85c5b5943cb04a19fcb72289bbe5b225e4dbbb
Griffith <probablyaplebeian@protonmail.com>
2024-12-13 12:42:20 -0800

    add fRequireStandardTx setup code to test_nexa

--- src/test/test_nexa.cpp
+++ src/test/test_nexa.cpp
@@ -44,6 +44,7 @@ extern void noui_connect();
 
 extern CTweak<uint32_t> limitFreeRelay;
 extern uint64_t nMaxSendBufferSize;
+extern std::atomic<bool> fRequireStandardTx;
 
 BasicTestingSetup::BasicTestingSetup(const std::string &chainName)
 {
@@ -61,6 +62,30 @@ BasicTestingSetup::BasicTestingSetup(const std::string &chainName)
     fCheckBlockIndex = true;
     SelectParams(chainName);
     noui_connect();
+
+    // set the global atomic fRequireStandardTx to what the params for the chain require.
+    fRequireStandardTx.store(Params().RequireStandard());
+    // check args to see if user wants to accept non standard txs
+    bool fAcceptNonStandard = GetBoolArg("-acceptnonstdtxn", !Params().RequireStandard());
+    // if the chain requires standard txs and
+    // the user wants to accept non standard txs
+    // return an error
+    if (fRequireStandardTx)
+    {
+        if (fAcceptNonStandard)
+        {
+            printf("acceptnonstdtxn is not currently supported for %s chain",
+                ModifiableParams().NetworkIDString().c_str());
+            assert(false);
+        }
+        // intentionally do nothing, the chain requires standard tx and
+        // the user configured to only allow standard tx
+    }
+    else
+    {
+        // otherwise set fRequireStandardTx to whatever the user configured
+        fRequireStandardTx.store(!fAcceptNonStandard);
+    }
 }
 
 BasicTestingSetup::~BasicTestingSetup() { ECC_Stop(); }
