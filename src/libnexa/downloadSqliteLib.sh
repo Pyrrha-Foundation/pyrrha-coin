10e0804bf44658fa37fa0397c8e5a70b1f75e83c
Andrew Stone <g.andrew.stone@gmail.com>
2025-01-31 17:50:38 -0500

    fix bignum fakeouts, update deps fixes to recent changes so that libnexa does not use boost when std is fine

--- src/libnexa/downloadSqliteLib.sh
+++ src/libnexa/downloadSqliteLib.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version=3460100
+version=3480000
 echo "Retrieving SQlite $version..."
 
 set -eu
@@ -8,13 +8,17 @@ set -eu
 dir_name=sqlite-amalgamation-${version}
 archive=sqlite-amalgamation-${version}.zip
 if [ ! -f "$archive" ]; then
-    curl -L "https://www.sqlite.org/2024/sqlite-amalgamation-$version.zip" -o $archive
+    curl -L "https://www.sqlite.org/2025/sqlite-amalgamation-$version.zip" -o $archive
     # wget -O $archive "https://www.sqlite.org/2023/sqlite-amalgamation-$version.zip"
 else
   echo "Archive $archive already downloaded"
 fi
 
-echo "77823cb110929c2bcb0f5d48e4833b5c59a8a6e40cdea3936b99e199dbbe5784  sqlite-amalgamation-3460100.zip" | sha256sum --check || { echo "sha256sum of sqlite3 failed"; exit 1; }
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    echo "skipping checksum check on macos"
+else
+    echo "d9a15a42db7c78f88fe3d3c5945acce2f4bfe9e4da9f685cd19f6ea1d40aa884 sqlite-amalgamation-3480000.zip" | sha256sum --check || { echo "sha256sum of sqlite3 failed"; exit 1; }
+fi
 
 echo "Extracting..."
 if [ ! -d "$dir_name" ]; then
