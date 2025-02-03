dce81978658bb22a092e4944ac353b9f5f6fa5cd
Peter Tschipper <peter.tschipper@gmail.com>
2025-02-03 17:11:53 -0400

    Update forktime to March 31, 12PM GMT

--- src/chainparams.h
+++ src/chainparams.h
@@ -17,9 +17,9 @@
 // Fork configuration
 /** This specifies the MTP time of the next fork */
 extern uint64_t nMiningForkTime;
-/** Next protocol upgrade will be activated once MTP >= March 15th 2025
+/** Next protocol upgrade will be activated once MTP >= 12:00:00 PM, March 31st 2025, GMT
  */
-const uint64_t NEXT_FORK_ACTIVATION_TIME = 1742040000;
+const uint64_t NEXT_FORK_ACTIVATION_TIME = 1743422400;
 
 /** Default for -minrelaytxfee, minimum relay fee for transactions */
 static const unsigned int DEFAULT_MIN_RELAY_TX_FEE = 1000;
